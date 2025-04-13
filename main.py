690926a028065f273aeeb0280788b48f686972a9
KarthiS25 <karthis1015@gmail.com>
2025-04-13 18:50:20 +0530

    Update the speech flow

--- main.py
+++ main.py
@@ -16,6 +16,21 @@ import sounddevice as sd
 import numpy as np
 import wave
 import streamlit as st
+import threading
+from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
+from IPython.display import Audio
+from datasets import load_dataset
+import torch
+import io
+import soundfile as sf
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
+audio_model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts").to(device)
+embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
+speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0).to(device)
+vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
+print("testtt")
 
 model = whisper.load_model("base")
 load_dotenv()
@@ -24,26 +39,6 @@ load_dotenv()
 SAMPLE_RATE = 16000  # Sample rate
 CHANNELS = 1  # Mono audio
 FILENAME = "recorded_audio.wav"
-
-# def record_audio():
-#     frames = []
-#     def callback(indata, frames_count, time, status):
-#         if status:
-#             print(status)
-#         frames.append(indata.copy())
-    
-#     with sd.InputStream(samplerate=SAMPLE_RATE, channels=CHANNELS, callback=callback):
-#         while st.session_state.get("mic_active", False):
-#             sd.sleep(100)  # Small delay to allow stopping
-    
-#     audio_data = np.concatenate(frames, axis=0)
-    
-#     # Save to WAV file
-#     with wave.open(FILENAME, 'wb') as wf:
-#         wf.setnchannels(CHANNELS)
-#         wf.setsampwidth(2)
-#         wf.setframerate(SAMPLE_RATE)
-#         wf.writeframes((audio_data * 32767).astype(np.int16).tobytes())
 
 # Transcribe audio
 def transcribe_audio(audio_data):
@@ -54,12 +49,16 @@ def transcribe_audio(audio_data):
 
 def text_to_speech(response):
     converter = pyttsx3.init()
-    converter.setProperty('rate', 180)
+    converter.setProperty('rate', 200)
     # converter.setProperty('volume', 0.8)
     converter.setProperty('voice', 'ta-IN')
     converter.say(response)
     # time.sleep(0.5)
     converter.runAndWait()
+    # inputs = processor(text=response, return_tensors="pt")
+    # speech = audio_model.generate_speech(inputs["input_ids"], speaker_embeddings, vocoder=vocoder)
+    # # st.audio(audio_bytes, format='audio/wav')
+    # sd.play(speech.numpy(), samplerate=16000)
 
 def get_audio_to_text():
     print("Recording...")
@@ -69,9 +68,6 @@ def get_audio_to_text():
 
       audio_data = r.record(source, duration=5)
       try:
-          # text = r.recognize_google(audio_data, language="en-IN")
-          # print(text, "text")
-          print(audio_data, "audio_data")
           data = transcribe_audio(audio_data)
           return data
       except sr.UnknownValueError:
@@ -124,7 +120,7 @@ if "chat_history" not in st.session_state or reset_history:
     st.session_state.chat_history = [{"role": "assistant", "content": st.session_state.starter_message}]
 
 def get_response(system_message, chat_history, user_text, 
-                 eos_token_id=['User'], max_new_tokens=256, get_llm_hf_kws={}):
+                eos_token_id=['User'], max_new_tokens=256, get_llm_hf_kws={}):
     # Set up the model
     hf = get_llm_hf_inference(max_new_tokens=max_new_tokens, temperature=0.1)
 
@@ -151,82 +147,59 @@ def get_response(system_message, chat_history, user_text,
     return response, chat_history
 
 # Chat interface
-# chat_interface = st.container(border=True)
-# with chat_interface:
-#     output_container = st.container()
-#     text = st.chat_input(placeholder="Enter your text here.")
-#     print(text, "Tetttt")
-#     st.session_state.user_text = text
-
-# Chat interface
 chat_interface = st.container(border=True)
 
 with chat_interface:
-    output_container = st.container()
-    col1, col2 = st.columns([10, 1])
-    with col1:
-      user_input = st.chat_input(
-          placeholder="Enter your text here or speak..."
-      )
-    with col2:
-    #   mic_button = st.button("🎤")
-    #   data = ""
-    #   audio_input = ''
-      audio_input = speech_to_text(language='en-IN', start_prompt="🎤", stop_prompt="🔴", just_once=False, use_container_width=False,callback=None,args=(),kwargs={},key=None)
-      print(audio_input, "AUdioooo")
+  output_container = st.container()
+  col1, col2 = st.columns([10, 1])
+  with col1:
+    user_input = st.chat_input(
+      placeholder="Enter your text here or speak..."
+    )
+  with col2:
+    audio_input = speech_to_text(language='en-IN', start_prompt="🎤", stop_prompt="🔴", just_once=False, use_container_width=False,callback=None,args=(),kwargs={},key=None)
 
-    # print(mic_button, "mic_buttonmic_buttonmic_buttonmic_buttonmic_button")
-    # if mic_button:
-    #     audio_input = get_audio_to_text()
-    # if data:
-    #     audio_input = transcribe_audio(data)
+  if audio_input:
+    st.session_state.user_text = audio_input
 
-    if audio_input:
-        print(audio_input, "AUdioooo")
-        st.session_state.user_text = audio_input
-
-    if user_input:
-        st.session_state.user_text = user_input
+  if user_input:
+    st.session_state.user_text = user_input
 
 # Display chat messages
 with output_container:
-    # For every message in the history
-    for message in st.session_state.chat_history:
-        # Skip the system message
-        if message['role'] == 'system':
-            continue
-            
-        # Display the chat message using the correct avatar
-        with st.chat_message(message['role'], 
-                             avatar=st.session_state['avatars'][message['role']]):
-            st.markdown(message['content'])
-
-        # if st.session_state.starter_message:
-        #     text_to_speech(st.session_state.starter_message)
-            
- # When the user enter new text:
-    if st.session_state.user_text:
+  # For every message in the history
+  for message in st.session_state.chat_history:
+    # Skip the system message
+    if message['role'] == 'system':
+      continue
         
-        # Display the user's new message immediately
-        with st.chat_message("user", 
-                             avatar=st.session_state.avatars['user']):
-            st.markdown(st.session_state.user_text)
-            
-        # Display a spinner status bar while waiting for the response
-        with st.chat_message("assistant", 
-                             avatar=st.session_state.avatars['assistant']):
-            # if st.session_state.starter_message:
-            #     text_to_speech(st.session_state.starter_message)
-            # print(st.chat_message("assistant"), "st.chat_message(assistant)")
+    # Display the chat message using the correct avatar
+    with st.chat_message(message['role'], 
+                          avatar=st.session_state['avatars'][message['role']]):
+      st.markdown(message['content'])
 
-            with st.spinner("Thinking..."):
-                # Call the Inference API with the system_prompt, user text, and history
-                response, st.session_state.chat_history = get_response(
-                    system_message=st.session_state.system_message, 
-                    user_text=st.session_state.user_text,
-                    chat_history=st.session_state.chat_history,
-                    max_new_tokens=st.session_state.max_response_length,
-                )
-                st.markdown(response)
-                print(response, "Response")
-            text_to_speech(response)
+    if len(st.session_state.chat_history) == 1 and not st.session_state.user_text:
+      text_to_speech(st.session_state.starter_message)
+            
+  # When the user enter new text:
+  if st.session_state.user_text:
+    # Display the user's new message immediately
+    with st.chat_message("user", 
+                          avatar=st.session_state.avatars['user']):
+      st.markdown(st.session_state.user_text)
+        
+    # Display a spinner status bar while waiting for the response
+    with st.chat_message("assistant", 
+                          avatar=st.session_state.avatars['assistant']):
+
+      with st.spinner("Thinking..."):
+        # Call the Inference API with the system_prompt, user text, and history
+        response, st.session_state.chat_history = get_response(
+            system_message=st.session_state.system_message, 
+            user_text=st.session_state.user_text,
+            chat_history=st.session_state.chat_history,
+            max_new_tokens=st.session_state.max_response_length,
+        )
+        st.markdown(response)
+        print(response, "Response")
+      text_to_speech(response)
